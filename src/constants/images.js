e8c2c09567bb7c1d0d8153d9dfe1ab90e8083322
Nkiriobasi <obasinkiri@gmail.com>
2022-06-16 12:28:09 +0100

    deleted unused images

--- src/constants/images.js
+++ src/constants/images.js
@@ -4,14 +4,12 @@ import api from '../assets/api.png';
 import cpp from '../assets/cpp.png';
 import css from '../assets/css.png';
 import figma from '../assets/figma.png';
-import flutter from '../assets/flutter.png';
 import git from '../assets/git.png';
 import graphql from '../assets/graphql.png';
 import html from '../assets/html.png';
 import javascript from '../assets/javascript.png';
 import mu5 from '../assets/mu5.png';
 import node from '../assets/node.png';
-import python from '../assets/python.png';
 import react from '../assets/react.png';
 import redux from '../assets/redux.png';
 import sass from '../assets/sass.png';
@@ -27,14 +25,6 @@ import about03 from '../assets/about03.png';
 import profile from '../assets/profile.jpg';
 import circle from '../assets/circle.svg';
 
-import adidas from '../assets/adidas.png';
-import amazon from '../assets/amazon.png';
-import asus from '../assets/asus.png';
-import bolt from '../assets/bolt.png';
-import nb from '../assets/nb.png';
-import skype from '../assets/skype.png';
-import spotify from '../assets/spotify.png';
-
 import netflix from '../assets/netflix.png';
 import foodRecipe from '../assets/foodRecipe.png';
 
@@ -46,14 +36,12 @@ export default {
   cpp,
   css,
   figma,
-  flutter,
   git,
   graphql,
   html,
   javascript,
   mu5,
   node,
-  python,
   react,
   redux,
   sass,
@@ -66,13 +54,6 @@ export default {
   about03,
   profile,
   circle,
-  adidas,
-  amazon,
-  asus,
-  bolt,
-  nb,
-  skype,
-  spotify,
   netflix,
   foodRecipe
 };

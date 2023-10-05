76feefb29c37e325f9c306919e65c4796f790fe8
Nkiriobasi <obasinkiri@gmail.com>
2023-10-05 16:27:07 +0100

    created new project

---
+++ app/layout.tsx
@@ -0,0 +1,46 @@
+import "./globals.css";
+import React, { ReactNode } from "react";
+import type { Metadata } from "next";
+import { Syne } from "next/font/google";
+
+const syne = Syne({
+    subsets: ["latin"],
+    display: "block",
+    weight: ["400", "500", "600", "700", "800"],
+});
+
+export const metadata: Metadata = {
+    title: "Frontend Engineer (Web | Mobile)",
+    generator: "Next.js",
+    colorScheme: "dark",
+    robots: {
+        index: true,
+        follow: true,
+        nocache: false,
+        googleBot: {
+            index: true,
+            follow: false,
+            noimageindex: true,
+            "max-video-preview": -1,
+            "max-image-preview": "large",
+            "max-snippet": -1,
+        },
+    },
+    category: "technology",
+};
+
+type RootLayoutProps = {
+  children: ReactNode;
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
+    return (
+        <html lang="en">
+            <body
+                className={`${syne.className} scroll-smooth scrollbar-none scrollbar-track-[#0E1016] scrollbar-thumb-[#212531]`}
+            >
+                {children}
+            </body>
+        </html>
+    );
+}

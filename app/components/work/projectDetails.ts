be81cbc185d7d677733654cf24850a818085dcab
Nkiriobasi <obasinkiri@gmail.com>
2023-10-06 14:39:53 +0100

    refactored projects I have worked on

--- app/components/work/projectDetails.ts
+++ app/components/work/projectDetails.ts
@@ -1,13 +1,9 @@
 import {
-    SiCplusplus,
     SiFramer,
-    SiGithub, SiNeovim,
     SiNextdotjs,
     SiReact,
-    SiRust,
     SiTailwindcss,
     SiTypescript,
-    SiZig
 } from "react-icons/si";
 import {IconType} from "react-icons";
 
@@ -29,26 +25,13 @@ export const projects = [
         id: 0,
         name: "Portfolio 2023",
         description:
-            "This is the fifth iteration of my portfolio.",
+            "This is my portfolio, showcasing my capabilities",
         technologies: [SiTypescript, SiReact, SiNextdotjs, SiTailwindcss, SiFramer],
         techNames: ["TypeScript", "React", "Next.js", "Tailwind CSS", "Framer Motion"],
         techLinks: ["https://www.typescriptlang.org/", "https://reactjs.org/", "https://nextjs.org/", "https://tailwindcss.com/", "https://www.framer.com/motion/"],
-        github: "https://github.com/nuIIpointerexception/www.seekvisualartist.com",
-        demo: "https://www.seekvisualartist.com/",
+        github: "https://github.com/Nkiriobasi/portfolio",
+        demo: "https://nkiriobasi.vercel.app/",
         image: "/projects/portfolio.webp",
-        available: true,
-    },
-    {
-        id: 1,
-        name: "Code Wars Solutions",
-        description:
-            "Just a couple of solutions for www.codewars.com problems. I'm currently ranked 7 kyu.",
-        technologies: [SiCplusplus, SiRust, SiNeovim],
-        techNames: ["C++", "Rust", "Neovim"],
-        techLinks: ["https://cplusplus.com/", "https://www.rust-lang.org/", "https://neovim.io/"],
-        github: "https://github.com/nuIIpointerexception/codewars",
-        demo: "https://www.codewars.com/",
-        image: "/projects/codewars.webp",
         available: true,
     },
     {
@@ -56,11 +39,11 @@ export const projects = [
         name: "Coming Soon",
         description:
             "I'm currently working on a couple of projects. I'll update this section as soon as I'm done.",
-        technologies: [SiZig],
-        techNames: ["Zig"],
-        techLinks: ["https://www.ziglang.org/"],
-        github: "https://github.com/nuIIpointerexception/",
-        demo: "https://github.com/nuIIpointerexception/",
+        technologies: [SiNextdotjs],
+        techNames: ["Next.js"],
+        techLinks: ["https://nextjs.org/"],
+        github: "https://github.com/Nkiriobasi/",
+        demo: "https://github.com/Nkiriobasi/",
         image: "/projects/construction.webp",
         available: false,
     },
